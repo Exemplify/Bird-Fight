504dc41fe0886b8388d6c51adcaafb5632e9bd85
Unknown <lautenbach.k@gmail.com>
2018-01-27 23:45:25 +0200

    Win con added, fixed static issues.

--- Assets/Scripts/Enumerators.cs
+++ Assets/Scripts/Enumerators.cs
@@ -3,3 +3,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum Players { P1, P2, P3, P4 }
+public enum Gamestate { Playing, Reset, Gameover }
--- Assets/Scripts/MailBox.cs
+++ Assets/Scripts/MailBox.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MailBox : MonoBehaviour {
 
+    public GameObject controller;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,7 +20,7 @@ public class MailBox : MonoBehaviour {
         if(other.gameObject.tag == "Mail")
         {
             Debug.Log("Player " + other.gameObject.GetComponent<Letter>().Owner.GetComponent<BirdController>().playerNumber + " has scored!");
-            ScoreController.AddScore(other.gameObject.GetComponent<Letter>().Owner.GetComponent<BirdController>().playerNumber);
+            controller.GetComponent<ScoreController>().AddScore(other.gameObject.GetComponent<Letter>().Owner.GetComponent<BirdController>().playerNumber);
 
             //Respawn letter
             other.gameObject.GetComponent<LetterController>().LetterRespawn();
